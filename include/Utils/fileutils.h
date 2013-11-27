d7e751ad627aa140bddda3f07f3ffca76fc49846
Peihong Guo <peihongguo@gmail.com>
2013-11-27 01:28:12 -0600

    added a write to file function

--- include/Utils/fileutils.h
+++ include/Utils/fileutils.h
@@ -10,3 +10,21 @@ string getPath( const string& filename );
 
 /* get the file name given a full path */
 string getFileName( const string& filename );
+
+/* write a iteratable item to file */
+template <typename T>
+bool write2file(const T& v, const string& filename)
+{
+	try{
+		ofstream fout(filename, ios::out);
+		for(auto it=v.begin();it!=v.end();it++) {
+			fout << (*it);
+		}
+		fout.close();
+		return true;
+	}
+	catch(...) {
+		cerr << "Failed to write to file " << filename << endl;
+		return false;
+	}
+}
